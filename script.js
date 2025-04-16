94008506d517c821a3f598b4290895db6e91fb1b
FlappyRug <andrewzarytskyi@gmail.com>
2025-04-16 16:46:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.play-button').addEventListener('click', () => {
+  console.log('Play button clicked');
+});
+document.querySelectorAll('.menu-button').forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    switch(i) {
+      case 0: console.log('Customize'); break;
+      case 1: console.log('Quests'); break;
+      case 2: console.log('Friends'); break;
+      case 3: console.log('Leaderboard'); break;
+      case 4: console.log('Shop'); break;
+    }
+  });
+});
